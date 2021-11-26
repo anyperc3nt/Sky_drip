c8336a5c1b2181c39730f79a2a29653a1fb42894
anypercent <kuruts.ae@phystech.edu>
2021-11-26 11:24:12 +0300

    added motionblur

--- project/graphics.py
+++ project/graphics.py
@@ -11,29 +11,43 @@ def init():
 
     global layer_curr
     layer_curr = pygame.Surface((Xscreensize, Yscreensize))
+    layer_curr = layer_curr.convert_alpha()
+    layer_curr.fill((0,0,0,0))
+
+    global layer_motionblur
+    layer_motionblur = pygame.Surface((Xscreensize, Yscreensize))
+    layer_motionblur = layer_motionblur.convert_alpha()
+    layer_motionblur.fill((0,0,0,0))
+
+    global layer_black
+    layer_black = pygame.Surface((Xscreensize,Yscreensize))
+    layer_black = layer_black.convert_alpha()
+    layer_black.fill((0,0,0,80))
 
 
 def draw_star(coords, brightness):
     """отрисовывает звезду заданной яркости в точке x, y экрана"""
 
-    brightness = int(brightness)
-
-    print(brightness)
-
     x = coords[0]
     y = coords[1]
 
-    circle(layer_curr, (brightness, brightness, brightness), (x, y), 1)
+    circle(layer_curr, (250, 250, 250), (x, y), 1)
 
 
 def update():
     global layer_curr
+    global layer_motionblur
+
+    pygame.Surface.blit(layer_motionblur,layer_black,(0,0))
+    pygame.Surface.blit(layer_motionblur,layer_curr,(0,0))
+    pygame.Surface.set_alpha(layer_motionblur, 230)
 
     screen.fill((0, 0, 0))
 
     pygame.Surface.blit(screen, layer_curr, (0, 0))
+    pygame.Surface.blit(screen, layer_motionblur, (0, 0))
 
     pygame.display.update()
 
     # стирание слоя перед след кадром
-    layer_curr.fill((0, 0, 0))
+    layer_curr.fill((0, 0, 0,0))
