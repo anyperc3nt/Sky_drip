8aee0aff7db26e311081dafe6b1c03b8dc4a829f
anypercent <kuruts.ae@phystech.edu>
2021-11-27 17:34:51 +0300

    added some docs and pep8 formatting

--- project/settings.py
+++ project/settings.py
@@ -1,4 +1,12 @@
-
 """настройки экрана"""
 Xscreensize = 1280
 Yscreensize = 720
+
+"""настройки размытия в движении"""
+motionblur_brightness = 200
+motionblur_force = 215
+
+"""настройка свечения звезд"""
+glow_size = 25
+glow_blur = 15
+glow_brightness = 30
