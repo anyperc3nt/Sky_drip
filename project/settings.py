4ab31e6c566c853bed6cf13f48c04c3218e01717
anyperc3nt <91312343+anyperc3nt@users.noreply.github.com>
2021-11-30 20:03:42 +0300

    add fullscreen mode

--- project/settings.py
+++ project/settings.py
@@ -1,14 +1,12 @@
-
 """настройки экрана"""
-
-Xscreensize = 1280
-Yscreensize = 720
+#Xscreensize = 1280
+#Yscreensize = 720
 
 """настройки размытия в движении"""
-motionblur_brightness=200
-motionblur_force=215
+motionblur_brightness = 200
+motionblur_force = 215
 
 """настройка свечения звезд"""
-glow_size=10
-glow_blur=10
-glow_brightness=40
+glow_size = 25
+glow_blur = 15
+glow_brightness = 30
