661ad0eb29ec3eefbafd517d75da9397bcaf4a93
anyperc3nt <91312343+anyperc3nt@users.noreply.github.com>
2021-12-17 05:37:35 +0300

    add interface and some refactoring

--- project/data.py
+++ project/data.py
@@ -4,13 +4,15 @@ import pandas as pd
 from collections import namedtuple
 import math
 
-
-
+#тапл для хранения данных о звездах
 Star = namedtuple('Star', ['phi', 'theta', 'brightness', 'name', 'id'])
+#тапл для передаче графиге данных о звездах
 Star_img = namedtuple('Star', ['x1', 'y1', 'brightness', 'name', 'id'])
+# x1, y1 = линейные координаты звезд, нормированные к еденице
 
 
 def coord2angles(x, y, z):
+    """конвертирует трехмерные координаты в полярные"""
     phi = np.arctan2(z, x)  # horizontal
     theta = np.arctan2(y, np.sqrt(x ** 2 + z ** 2))  # vertical (declination)
     return phi, theta
@@ -37,14 +39,17 @@ def init():
 
     stars = []
 
-    # самая яркая звезда сириус, у нее магнитуда -1,67
-    # по этому мы берем -mag от -6,5 до 1,67
-    # а еще я не беру звезды ярче сириуса, потому что в датасете походу есть солнце и другие слишком яркие объекты
+    """
+    самая яркая звезда сириус, у нее магнитуда -1,67
+    по этому мы берем -mag от -6,5 до 1,67
+    а еще не берем звезды ярче сириуса, потому что в датасете видимо есть солнце и другие слишком яркие объекты
+    """
 
     for i in range(num_stars):
         if ((-mag[i]) > -6.5) and (-mag[i] < 1.68):
             s_brightness = (6.5 - mag[i]) / (1.68 + 6.5) * 255
-            stars.append(Star(phi[i], theta[i], s_brightness, names[i], ids[i]))
+            stars.append(
+                Star(phi[i], theta[i], s_brightness, names[i], ids[i]))
 
 
 def what_we_see(hor_angle, vert_angle, visual_field, Time):
@@ -52,24 +57,24 @@ def what_we_see(hor_angle, vert_angle, visual_field, Time):
 
     hor_angle, vert_angle - углы направления зрения пользователя в радианах
     visual_field - угол обзора пользователя
+    Time - время, от него зависит расположение звезд
     """
     global stars
 
     delta_w = 0.00001
+    delta_h = 0.00001
 
     visible_stars = []
 
     for a_star in stars:
-
-        theta = a_star.theta - vert_angle
-        phi = a_star.phi - hor_angle + delta_w*Time*abs(math.cos(vert_angle))
+        theta = a_star.theta - vert_angle + delta_h*Time
+        phi = a_star.phi - hor_angle + delta_w*Time
         x1 = (math.tan(phi) / math.tan(visual_field / 2) + 1) / 2
         y1 = (math.tan(theta) / math.tan(visual_field / 2) + 1) / 2
 
         if 0 <= x1 <= 1 and 0 <= y1 <= 1:
-
-            visible_star = Star_img(x1, y1, a_star.brightness, a_star.name, a_star.id)
-
+            visible_star = Star_img(
+                x1, y1, a_star.brightness, a_star.name, a_star.id)
             visible_stars.append(visible_star)
 
     return visible_stars
